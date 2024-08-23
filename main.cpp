a9def89bd2249a1fa2952dfa83827d8eb67679fc
conniewastaken <85717828+conniewastaken@users.noreply.github.com>
2024-08-22 20:30:29 -0700

    Added function prototypes and functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
-//
-//  main.cpp
-//  TicTacToe
-//
-//  Created by connie zeng on 8/22/24.
-//
-
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int main() {
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+int main() 
+{
+    
+    return 0;
+    
+}
+void drawBoard(char *spaces) {
+    
+}
+void playerMove(char *spaces, char player){
+    
+}
+void computerMove(char *spaces, char computer){
+    
+}
+bool checkWinner(char *spaces, char player, char computer){
+    return 0;
+}
+bool checkTie(char *spaces){
+    return 0;
 }
