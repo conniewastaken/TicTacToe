4458a51d8d680aefdea8a0e832e500d44dcd7447
conniewastaken <85717828+conniewastaken@users.noreply.github.com>
2024-08-22 20:50:42 -0700

    Declared variables in the main function

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ bool checkTie(char *spaces);
 
 int main() 
 {
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+    
     
     return 0;
     
