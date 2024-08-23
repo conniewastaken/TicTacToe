d11033672d7b6b098783ee3d6435fa728ee832a9
conniewastaken <85717828+conniewastaken@users.noreply.github.com>
2024-08-22 21:14:54 -0700

    drew the tictactoe board

--- main.cpp
+++ main.cpp
@@ -15,12 +15,24 @@ int main()
     char computer = 'O';
     bool running = true;
     
+    drawBoard(spaces);          /* when passing an array through a function
+                                it's a pointer already */
     
     return 0;
     
 }
 void drawBoard(char *spaces) {
-    
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "   " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "   " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "   " << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
 }
 void playerMove(char *spaces, char player){
     
@@ -34,3 +46,4 @@ bool checkWinner(char *spaces, char player, char computer){
 bool checkTie(char *spaces){
     return 0;
 }
+
